b4eb52118cd6c0c47c85d50a51dda5f88e67570e
Gerardo Maruotti <maruotti@icloud.com>
2022-05-08 11:24:22 +0200

    Add IoT Technologies folder and first project

---
+++ Tecnologie per IoT/mqtt_publisher.py
@@ -0,0 +1,54 @@
+import numpy as np
+import paho.mqtt.client as MQTT
+import time #errore import libreria time
+import json #errore import libreria json
+
+class Publisher:
+    def __init__(self, broker, port, topic, clientID):
+        self.broker = broker
+        self.clientID = clientID
+        self.topic = topic
+        self.port = port
+        self.client = MQTT.Client();
+        self.message = ""
+        self.client.on_connect = self.onConnect() #client error not MQTT
+
+    def onConnect(self):
+        print("Connected to broker: " + self.broker)
+
+    def start(self):
+        self.client.connect(self.broker, self.port) #client error not MQTT
+        self.client.loop_start() #client error not MQTT
+
+    def publish(self, topic, message):
+        self.client.publish(topic, json.dumps(message), 2) #client error not MQTT e trasformazione dictionary in stringa
+
+    def stop(self):
+        self.client.disconnect() #client error not MQTT
+        self.client.loop_stop() #client error not MQTT
+
+    def encode(self, v, n, u):
+        self.message = {
+            "bn": self.clientID,
+            "e": [{
+                "n": n,
+                "u": u,
+                "t": time.time(), #time.time()
+                "v": v
+            }]
+        }
+
+if __name__ == "__main__":
+
+    broker = "127.0.0.1"
+    port = 1883
+    clientID = "/tiot/devices/1234"
+    topic = "/tiot/devices/1234"
+    device = Publisher(broker, port, topic, clientID)
+    device.start() #errore parentesi e chiamata funzione ()
+
+    while True:
+        temp = np.random.uniform(15,30,1)[0]
+        device.encode(temp, "temperature", "C")
+        device.publish(topic, device.message)
+        time.sleep(600) #il tempo della sleep e da intendersi come secondi (Errore commenti)
